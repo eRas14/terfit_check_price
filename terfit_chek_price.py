83b3c4eb9764c040431ddc8f2c96cc68a8435dc6
Urazov Aibek <ibek-94@mail.ru>
2025-01-14 18:03:45 +0500

    Добавлена цена без скидки и размер скидки

--- terfit_chek_price.py
+++ terfit_chek_price.py
@@ -4,17 +4,20 @@ from selenium.webdriver.common.by import By
 
 
 browser = webdriver.Firefox()
-browser.implicitly_wait(10)
+browser.implicitly_wait(2)
 browser.get("https://terfit.ru/cards-shop/orenburg/")
 
-#Тут лежат все названия
+#Список с названием абонементов
 price_card_name = browser.find_elements(By.CSS_SELECTOR, ".catalog-card__title")
 
-#Тут лежат все цены
+#Список с ценами без скидки
+price_card_list_promo = browser.find_elements(By.CSS_SELECTOR, ".catalog-card__price-promo")
+
+#Список с ценами со скидкой
 price_card_list = browser.find_elements(By.CSS_SELECTOR, ".catalog-card__price")
 
 #Выводим цену всех абонементов циклом:
 for i in range(len(price_card_name)):
-    print(f"Цена {price_card_name[i].text}: {price_card_list[i].text}")
+    print(f"Актуальная цена {price_card_name[i].text}: {price_card_list[i].text}, цена без скидки {price_card_list_promo[i].text}")
 
 browser.quit()
